40d8e86f5153525cc4b786cb96d5aac96f7eb533
David Mihal <david@dmihal.com>
2022-09-22 19:46:35 -0500

    Test successful commmitment

--- src/test/L2Controller.t.sol
+++ src/test/L2Controller.t.sol
@@ -62,7 +62,52 @@ contract L2ControllerTest is DSTest {
         l2Controller.withdrawCommitment(commitment);
 
         commitmentData = l2Controller.getCommitment(commitment);
+
+        assertEq(commitmentData.creator, address(0));
+        assertEq(commitmentData.feeToken, address(0));
+        assertEq(commitmentData.feeAmount, 0);
+        assertEq(commitmentData.withdrawTime, 0);
+
         assertEq(utils.dai().balanceOf(vitalik.addr()), vitalikStartDaiBalance - 10 ether);
+        assertEq(l2Controller.depositted(vitalik.addr(), address(utils.dai())), 10 ether);
+
+        vm.stopPrank();
+    }
+
+    function testCommitmentCompleted() public {
+        User memory vitalik = users[1];
+        User memory relayer = users[2];
+        uint256 vitalikStartDaiBalance = utils.dai().balanceOf(vitalik.addr());
+        uint256 relayerStartDaiBalance = utils.dai().balanceOf(relayer.addr());
+        bytes32 commitment = keccak256("Commitment");
+
+        vm.startPrank(vitalik.addr());
+
+        utils.dai().approve(address(l2Controller), 20 ether);
+        l2Controller.createCommitment(commitment, address(utils.dai()), 10 ether, 10 ether);
+
+        L2Controller.Commitment memory commitmentData = l2Controller.getCommitment(commitment);
+        assertEq(commitmentData.creator, vitalik.addr());
+        assertEq(commitmentData.feeToken, address(utils.dai()));
+        assertEq(commitmentData.feeAmount, 10 ether);
+        assertEq(commitmentData.withdrawTime, 0);
+
+        assertEq(utils.dai().balanceOf(vitalik.addr()), vitalikStartDaiBalance - 20 ether);
+        assertEq(l2Controller.depositted(vitalik.addr(), address(utils.dai())), 20 ether);
+
+        vm.stopPrank();
+
+        vm.startPrank(bridge.addr());
+
+        l2Controller.recordCommitment(relayer.addr(), abi.encodePacked(commitment));
+
+        commitmentData = l2Controller.getCommitment(commitment);
+        assertEq(commitmentData.creator, address(0));
+        assertEq(commitmentData.feeToken, address(0));
+        assertEq(commitmentData.feeAmount, 0);
+        assertEq(commitmentData.withdrawTime, 0);
+
+        assertEq(utils.dai().balanceOf(relayer.addr()), relayerStartDaiBalance + 10 ether);
         assertEq(l2Controller.depositted(vitalik.addr(), address(utils.dai())), 10 ether);
 
         vm.stopPrank();
